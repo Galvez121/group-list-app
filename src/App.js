a74c7764db70452c90e0ed3162b408808df671b6
Juan Galvez <juanjosegalvez2014@gmail.com>
2024-01-26 15:07:16 -0500

    first commit - basic behavior created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
+const friendTest = [
+  {
+    name: "Juan",
+    age: 20,
+    birhday: "1/26/2024",
+    key: 0,
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="container">
+      <FormAddfriend />
+    </main>
   );
 }
 
-export default App;
+function FormAddfriend() {
+  const [friendData, setFriendData] = useState([]);
+  const [friendName, setFriendName] = useState("");
+
+  function handleFriendList(friendList) {
+    setFriendData((friend) => [...friend, friendList]);
+  }
+
+  function hanndleSubmit(e) {
+    e.preventDefault();
+
+    // It will create a new object
+    const newFriend = { friendName, id: Date.now() };
+
+    handleFriendList(newFriend);
+  }
+
+  return (
+    <>
+      <h1>Add a friend</h1>
+      <form onSubmit={hanndleSubmit}>
+        <label for="friendName">Name:</label>
+        <input
+          type="text"
+          id="friendName"
+          name="friendName"
+          placeholder="Enter friend's name"
+          onChange={(e) => setFriendName(e.target.value)}
+          required
+        />
+
+        <label for="friendDescription">Description:</label>
+        <textarea
+          id="friendDescription"
+          name="friendDescription"
+          placeholder="Enter friend's description"></textarea>
+
+        <button type="submit">Add Friend</button>
+      </form>
+      <Friend friendData={friendData} />
+    </>
+  );
+}
+
+// function FriendList() {}
+
+function Friend({ friendData }) {
+  return (
+    <>
+      <h2>List of friends</h2>
+      <ul>
+        {friendData.map((friend) => (
+          <FriendList friendItem={friend} key={friend.id} />
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function FriendList({ friendItem }) {
+  return <li>{friendItem.friendName}</li>;
+}
